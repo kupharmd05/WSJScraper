695e0a760fe64da380bc333e23f29490dbc51af7
kupharmd05 <kupharmd05@gmail.com>
2019-04-03 22:30:13 -0500

    All working except save button

--- public/app.js
+++ public/app.js
@@ -1,10 +1,54 @@
+$(document).ready(function () {
 
+  function getArticles() {
+  $.getJSON("/articles", function (data) {
+    // For each one
+    for (var i = 0; i < 20; i++) {
+      // Display the information on the page
+      $("#articles").append("<div class='article'><a href =" + data[i].link + "<p class='articleTitle'>" + data[i].title + "</a><br><p class='summary'>"+data[i].summary +"</p>" + "<a class='btn btn-success save' data-id='"+data[i]._id +"'> Save </a></p></div>");
+
+    }}
+  )};
+
+  $(".new-scrape").on("click", function (event) {
+    event.preventDefault();
+    $("#articles").empty();
+    $.get("/scrape").then(function (data) {
+      console.log(data);
+      getArticles();
+      
+    })
+  })
+  $(".clear").on("click", (event) => {
    
+  
+    $.ajax({
+      method: "GET",
+      dataType: "json",
+      url: "/clear",
+      success: function(response){
+        $("#articles").empty();
+      }
+    
+    });
 
-    $.getJSON("/articles", function(data) {
-        // For each one
-        for (var i = 0; i < data.length; i++) {
-          // Display the apropos information on the page
-          $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-        }
-      });
+  });
+
+  $(document).on("click",".save", (event) => {
+    alert("this hit")
+    var articleToSave = $(this).attr("data-id");
+
+    $.ajax({
+      method: "POST",
+      url: "/saved/" + articleToSave
+    }).then(function(data){
+      console.log(data);
+    })
+  })
+
+
+getArticles();
+})
+
+
+
